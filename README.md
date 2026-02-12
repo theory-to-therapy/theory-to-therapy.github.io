a136adfea5e2036236b26db8ed279e9ce5bb1b1f
theory-to-therapy <m.wietersheim.kramsta@gmail.com>
2026-02-12 12:33:05 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Conference Website Template
+
+A simple static website designed for conferences or events.
+
+[Check the live demo here!](https://nahimsouza.github.io/conference-website).
+
+## For Developers
+
+This template is built with simplicity in mind, the pages were built just with HTML, CSS (Flexbox) and a few lines of JavaScript. 
+
+* Header and footer sections are managed on [scripts.js](scripts.js) to avoid duplicating code across HTML files (not the perfect solution, but it gets the job done!).
+
+* To create a new page, just copy [template.html](template.html). It comes with the header and footer already in place, so you only need to fill in the main section.
+
+### Known Issues
+
+The responsive design is functional, but may require additional refinement for better performance across all devices.
+
+## Acknowledgements
+
+* A big shoutout to the [SG WEB DEV YouTube channel](https://www.youtube.com/@SGWEBDEV) for helping me get started from scratch. These two videos were especially helpful:
+
+    * [How to Make a Website Using CSS Flexbox](https://www.youtube.com/watch?v=SP1QMG6WPNk)
+    * [Learn to build a Responsive Website Using CSS Flexbox](https://www.youtube.com/watch?v=TId6holGVeY)
+
+* [ChatGPT](https://chatgpt.com/) was a helpful resource for insights and quick solutions, especially for small features like the dropdown menus.
---
+++ scripts.js
@@ -0,0 +1,59 @@
+
+function loadComponents() {
+    // This workaround loads the header and the footer, 
+    // and insert it into the proper divs (based on their IDs)
+    // to avoid duplication across multiple pages.
+    
+    header = `
+    <header>
+    <nav>
+        <a href="index.html">
+            <div class="logo">
+                <img src="images/the-amazing-conf-logo-blue-transparent.png" alt="Conference Logo" height="60px">
+            </div>
+        </a>
+        <ul class="main__ul">
+            <li><a href="index.html">Home</a></li>
+            <li><a href="dates.html">Important Dates</a></li>
+            <li><a href="#">Calls</a>
+                <ul class="dropdown">
+                    <li><a href="#">Track 1</a></li>
+                    <li><a href="#">Track 2</a></li>
+                    <li><a href="#">Track 3</a></li>
+                </ul>
+            </li>
+            <li><a href="#">Submissions</a></li>
+            <li><a href="#">Venue/Local</a>
+            <ul class="dropdown">
+                <li><a href="gallery.html">Gallery</a></li>
+                <li><a href="#">Item 2</a></li>
+                <li><a href="#">Item 3</a></li>
+            </ul>
+            </li>
+            <li><a href="#">Organization</a></li>
+            <li><a href="#">Program</a></li>
+            <li><a href="#">Sponsoring</a></li>
+            <li><a href="#" class="link-btn">Registration</a></li>
+        </ul>
+    </nav>
+    <div id='menu' class='box-icon'><i class='bx bx-menu'></i></div>
+    </header>
+    `
+
+    footer = `
+    <footer>
+        <p>© Copyright 2025. Conferece Website Template - Designed by <a href="https://github.com/nahimsouza/conference-website/">nahimsouza</a>. MIT License.</p>
+    </footer>
+    `
+
+    document.getElementById('header').innerHTML = header;
+    document.getElementById('footer').innerHTML = footer;
+
+    // responsive menu
+    document.querySelector('#menu').addEventListener('click', ()=>{
+        document.querySelector('nav ul').classList.toggle('showmenu');
+    });
+
+}
+
+window.onload = loadComponents;
